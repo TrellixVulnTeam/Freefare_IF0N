ad49de8f7a373fffd1a2af15761d1d3b2a40534c
Justo Karell <jek124@case.edu>
2022-06-14 21:27:05 -0400

    changed base dir to main/Templates

--- Sal_django_site/Sal_website/settings.py
+++ Sal_django_site/Sal_website/settings.py
@@ -83,7 +83,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         # 'DIRS': [],
-        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'main/Templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
