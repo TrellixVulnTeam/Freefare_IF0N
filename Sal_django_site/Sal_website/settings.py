ff2e6a47cc24f49b9bdf3e8e58d50eb0d1fdf80c
Justo Karell <jek124@case.edu>
2022-06-14 22:40:38 -0400

    changed PROJECT_DIR to BASE_DIR in static var defintions

--- Sal_django_site/Sal_website/settings.py
+++ Sal_django_site/Sal_website/settings.py
@@ -171,9 +171,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'main/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'main/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'main/static')
 STATICFILES_DIRS = [
     # os.path.join(PROJECT_DIR, 'static'),
     os.path.join(BASE_DIR, 'node_modules'),
