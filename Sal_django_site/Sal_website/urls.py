d5af0418c9fae0da4f166187796f20acdf705a14
Justo Karell <jek124@case.edu>
2022-06-18 16:23:13 -0400

    DEBUG=False, added dynamic urlpattern to serve static files

--- Sal_django_site/Sal_website/urls.py
+++ Sal_django_site/Sal_website/urls.py
@@ -31,6 +31,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api-auth/', include('drf_social_oauth2.urls',namespace='drf')),#???? namespace='drf' is mandatory
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve',
+    {'document_root', settings.STATIC_ROOT})
 
 
 
