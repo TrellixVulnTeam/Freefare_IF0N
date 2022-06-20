470014ad54b7ae4a0469163153559ad996174bdd
Justo Karell <jek124@case.edu>
2022-06-20 09:59:20 -0400

    reverting to when it worked

--- Sal_django_site/Sal_website/urls.py
+++ Sal_django_site/Sal_website/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-from django.conf.urls import patterns, include, url
+# from django.conf.urls import patterns, include, url
 from django.views.i18n import JavaScriptCatalog
 from django.conf.urls import url, include
 from django.contrib.auth import views as auth_views
@@ -41,8 +41,8 @@ urlpatterns = [
     
 ]
 
-if settings.DEBUG:
-    urlpatterns = patterns('',
-    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
-    url(r'', include('django.contrib.staticfiles.urls')),
-) + urlpatterns
+# if settings.DEBUG:
+#     urlpatterns = patterns('',
+#     url(r'^media/(?P<path>.*)$', 'django.views.static.serve',{'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+#     url(r'', include('django.contrib.staticfiles.urls')),
+# ) + urlpatterns
