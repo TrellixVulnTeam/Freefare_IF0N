1fd96fd448fc21d10d56ebc6f0f60ed56ed3ffb5
Justo Karell <jek124@case.edu>
2022-10-13 08:45:15 -0400

    edited test_valid_ url names with dash

--- Sal_django_site/main/tests.py
+++ Sal_django_site/main/tests.py
@@ -178,25 +178,25 @@ class ProfileTests(TestCase):
     
     # Test whether address field is greater than 3 characters
     def test_valid_org_address(self):
-        url = reverse('profile_view')
+        url = reverse('profile-view')
         response = self.client.get(url)
         self.assertTrue(re.search(r'<p class="p-line"><strong>Organzation Address: </strong>\.(.*?)</p>', response).group(1)>=3)
     
     # Test whether city field is non-empty
     def test_valid_org_address(self):
-        url = reverse('profile_view')
+        url = reverse('profile-view')
         response = self.client.get(url)
         self.assertTrue(re.search(r'<p class="p-line col"><strong> City: </strong>\.(.*?)</p>', response).group(1)!='')
     
     # Test whether state field is non-empty
     def test_valid_org_address(self):
-        url = reverse('profile_view')
+        url = reverse('profile-view')
         response = self.client.get(url)
         self.assertTrue(re.search(r'<p class="p-line col"><strong> State: </strong>\.(.*?)</p>', response).group(1)!='')
     
     # Test whether zipcode field is non-empty
     def test_valid_org_address(self):
-        url = reverse('profile_view')
+        url = reverse('profile-view')
         response = self.client.get(url)
         self.assertTrue(re.search(r'<p class="p-line col"><strong> Zipcode: </strong>\.(.*?)</p>', response).group(1)!='')
     
